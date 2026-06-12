c93cb76232b74149c8aed467a8ef526e283e4976
Diya <diyamaru32@gmail.com>
2026-06-12 19:23:54 +0530

    Create 1.js

---
+++ Javascript/Lab13/1.js
@@ -0,0 +1,6 @@
+//(1) Write a Node.js program using chalk to print colorful log messages.
+const chalk = require('chalk');
+console.log(chalk.green('Success!'));
+console.log(chalk.blue('Information'));
+console.log(chalk.red('Error!'));
+console.log(chalk.yellow('Warning!'));
